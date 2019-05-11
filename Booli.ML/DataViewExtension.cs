f303822a1b54a9df08f555d1bab5294dd563090b
Niklas Brännlund <niklas.brannlund@live.se>
2019-05-11 14:41:22 +0200

    added extension class for ml.net dataview

---
+++ Booli.ML/DataViewExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.DataView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booli.ML
+{
+  public static class DataViewExtension
+  {
+    public static string[] GetNumericalFeatureColumnNames(this IDataView dw) => dw.Schema
+                                                                      .Where(feature => feature.Name != "Label" && 
+                                                                             feature.Type.ToString() == "R4")
+                                                                      .Select(f => f.Name).ToArray();
+
+    public static string[] GetCategoricalFeatureColumnNames(this IDataView dw) => dw.Schema
+                                                                  .Where(feature => feature.Name != "Label" &&
+                                                                         feature.Type.ToString() == "Text")
+                                                                  .Select(f => f.Name).ToArray();
+  }
+}
