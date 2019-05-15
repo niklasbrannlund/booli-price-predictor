cc91c157135afdc8c01574b8a1e6b0d04bc2022d
Niklas Brännlund <niklas.brannlund@live.se>
2019-05-15 19:37:17 +0200

    added a simple test

---
+++ Booli.ML.Test/ModelTrainerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BooliAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Booli.ML.Test
+{
+  [TestClass]
+  public class ModelTrainerTest
+  {
+    private IAPIClient _client;
+    private string _area;
+
+    [TestInitialize]
+    public void Setup()
+    {
+      var apiClientMock = new Mock<IAPIClient>();
+      _client = apiClientMock.Object;
+
+      _area = "svedmyra";
+    }
+
+    [TestMethod]
+    public void CanConstruct()
+    {
+      var modeltrainer = new ModelTrainer(_client, _area);
+      Assert.IsNotNull(modeltrainer);
+    }
+  }
+}
---
+++ Booli.ML.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Booli.ML.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Booli.ML.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("287505ab-dc67-42b6-891a-8ee220ed0919")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
